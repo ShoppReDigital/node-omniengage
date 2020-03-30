26b92cae433f3ce69e036ec6e782e232df18c5ed
Manjesh V <manjeshpv@gmail.com>
2020-03-30 18:13:33 +0530

    added binary 0.0.7

--- ses.js
+++ ses.js
@@ -2,7 +2,7 @@
 exports.createTemplate = (payload) => {
     rp({
         method: 'POST',
-        url: `${process.env.OMNIENGAGE_URL}/api/ses/sendTemplatedEmail`,
+        url: `${process.env.OMNIENGAGE_URL}/api/ses/createTemplate`,
         json: true,
         body: payload,
     })
@@ -11,7 +11,7 @@ exports.createTemplate = (payload) => {
 exports.updateTemplate = (payload) => {
     rp({
         method: 'POST',
-        url:`${process.env.OMNIENGAGE_URL}/api/ses/sendTemplatedEmail`,
+        url:`${process.env.OMNIENGAGE_URL}/api/ses/updateTemplate`,
         json: true,
         body: payload,
     })
